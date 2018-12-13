d35f3a234a488131265fd641e14978ef616172cf
L <1252504314@qq.com>
2018-12-13 22:22:06 +0800

    update

--- sub-web/src/main/java/org/github/common/mapper/CommonMapper.java
+++ sub-web/src/main/java/org/github/common/mapper/CommonMapper.java
@@ -2,6 +2,9 @@ package org.github.common.mapper;
 
 import java.util.Date;
 
+import org.apache.ibatis.annotations.Select;
+
 public interface CommonMapper {
+  @Select("select now()")
   Date getNow();
 }
