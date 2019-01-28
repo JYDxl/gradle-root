a9c4555321ebe2bc006fe61c032ab0350ced2355
L <1252504314@qq.com>
2019-01-28 20:36:56 +0800

    update

---
+++ sub-netty/src/main/java/org/github/netty/handler/HttpFileServerChannelHandler.kt
@@ -0,0 +1,31 @@
+package org.github.netty.handler
+
+import io.netty.channel.ChannelFutureListener.CLOSE
+import io.netty.channel.ChannelHandler.Sharable
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.SimpleChannelInboundHandler
+import io.netty.handler.codec.http.DefaultFullHttpResponse
+import io.netty.handler.codec.http.FullHttpRequest
+import io.netty.handler.codec.http.HttpResponseStatus.OK
+import io.netty.handler.codec.http.HttpVersion.HTTP_1_1
+import org.slf4j.LoggerFactory.getLogger
+
+@Sharable
+class HttpFileServerChannelHandler : SimpleChannelInboundHandler<FullHttpRequest>() {
+  override fun channelActive(ctx: ChannelHandlerContext) {
+    log.info("ACTIVE")
+  }
+
+  override fun channelRead0(ctx: ChannelHandlerContext, req: FullHttpRequest) {
+    log.info(req.toString())
+    val resp = DefaultFullHttpResponse(HTTP_1_1, OK)
+    ctx.writeAndFlush(resp).addListener(CLOSE)
+  }
+
+  override fun channelInactive(ctx: ChannelHandlerContext) {
+    log.info("IN_ACTIVE")
+  }
+}
+
+/** log. */
+private val log = getLogger(HttpFileServerChannelHandler::class.java)!!
---
+++ sub-web/src/main/java/org/github/config/NettyConfig.kt
@@ -0,0 +1,121 @@
+package org.github.config
+
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.buffer.Unpooled.copiedBuffer
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.ChannelOption.SO_BACKLOG
+import io.netty.channel.epoll.EpollEventLoopGroup
+import io.netty.channel.epoll.EpollServerSocketChannel
+import io.netty.channel.epoll.EpollSocketChannel
+import io.netty.handler.codec.DelimiterBasedFrameDecoder
+import io.netty.handler.codec.FixedLengthFrameDecoder
+import io.netty.handler.codec.LineBasedFrameDecoder
+import io.netty.handler.codec.http.HttpObjectAggregator
+import io.netty.handler.codec.http.HttpRequestDecoder
+import io.netty.handler.codec.http.HttpResponseEncoder
+import io.netty.handler.codec.protobuf.ProtobufDecoder
+import io.netty.handler.codec.protobuf.ProtobufEncoder
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender
+import io.netty.handler.codec.string.StringDecoder
+import io.netty.handler.codec.string.StringEncoder
+import io.netty.handler.logging.LoggingHandler
+import io.netty.handler.stream.ChunkedWriteHandler
+import org.github.netty.handler.HttpFileServerChannelHandler
+import org.github.netty.handler.ServerChannelHandler
+import org.github.netty.protobuf.SubscribeReqProto.SubscribeReq
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Scope
+import kotlin.text.Charsets.UTF_8
+
+@Configuration
+class NettyConfig : InitializingBean {
+  @Scope(SCOPE_PROTOTYPE)
+  @Bean
+  fun lineBasedFrameDecoder() = LineBasedFrameDecoder(1024)
+
+  @Scope(SCOPE_PROTOTYPE)
+  @Bean
+  fun delimiterBasedFrameDecoder() = DelimiterBasedFrameDecoder(1024, true, false, copiedBuffer("\$_\n", UTF_8))
+
+  @Scope(SCOPE_PROTOTYPE)
+  @Bean
+  fun fixedLengthFrameDecoder() = FixedLengthFrameDecoder(20)
+
+  @Bean
+  fun stringDecoder() = StringDecoder()
+
+  @Bean
+  fun stringEncoder() = StringEncoder()
+
+  @Bean
+  fun serverChannelHandler() = ServerChannelHandler()
+
+  @Bean
+  fun loggingHandler() = LoggingHandler()
+
+  @Scope(SCOPE_PROTOTYPE)
+  @Bean
+  fun protobufVarint32FrameDecoder() = ProtobufVarint32FrameDecoder()
+
+  @Bean
+  fun protobufDecoder() = ProtobufDecoder(SubscribeReq.getDefaultInstance())
+
+  @Bean
+  fun protobufVarint32LengthFieldPrepender() = ProtobufVarint32LengthFieldPrepender()
+
+  @Scope(SCOPE_PROTOTYPE)
+  @Bean
+  fun httpRequestDecoder() = HttpRequestDecoder()
+
+  @Scope(SCOPE_PROTOTYPE)
+  @Bean
+  fun httpObjectAggregator() = HttpObjectAggregator(65536)
+
+  @Bean
+  fun protobufEncoder() = ProtobufEncoder()
+
+  @Scope(SCOPE_PROTOTYPE)
+  @Bean
+  fun httpResponseEncoder() = HttpResponseEncoder()
+
+  @Scope(SCOPE_PROTOTYPE)
+  @Bean
+  fun chunkedWriteHandler() = ChunkedWriteHandler()
+
+  @Bean
+  fun httpFileServerChannelHandler() = HttpFileServerChannelHandler()
+
+  override fun afterPropertiesSet() {
+    val boss = EpollEventLoopGroup()
+    val worker = EpollEventLoopGroup()
+    ServerBootstrap()
+      .group(boss, worker)
+      .channel(EpollServerSocketChannel::class.java)
+      .option(SO_BACKLOG, 1024)
+      .handler(loggingHandler())
+      .childHandler(object : ChannelInitializer<EpollSocketChannel>() {
+        override fun initChannel(channel: EpollSocketChannel) {
+          channel.pipeline()!!.apply {
+            addLast(loggingHandler())
+            addLast(httpRequestDecoder())
+            addLast(httpObjectAggregator())
+            addLast(httpResponseEncoder())
+            addLast(chunkedWriteHandler())
+            addLast(httpFileServerChannelHandler())
+          }
+        }
+      })
+      .bind(8090)
+      .sync()
+      .channel()
+      .closeFuture()
+      .addListener {
+        boss.shutdownGracefully()
+        worker.shutdownGracefully()
+      }
+  }
+}
