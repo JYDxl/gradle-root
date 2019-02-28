0845dd45d731b37ec15868732ff4e35b811bcbe6
L <1252504314@qq.com>
2019-02-28 23:44:52 +0800

    update

--- sub-web/src/main/java/org/github/config/EventBusConfig.kt
+++ sub-web/src/main/java/org/github/config/EventBusConfig.kt
@@ -1,14 +1,23 @@
 package org.github.config
 
 import com.google.common.eventbus.AsyncEventBus
+import com.google.common.util.concurrent.MoreExecutors
 import org.github.event.EventSubscriber
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.task.TaskExecutor
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 
 @Configuration
 class EventBusConfig {
   @Bean
   fun eventBus(@Qualifier("taskExecutor") executor: TaskExecutor, subscribers: List<EventSubscriber>) = AsyncEventBus(executor).apply { subscribers.forEach { register(it) } }
+
+  @Bean
+  fun listeningExecutor(@Qualifier("taskExecutor") executor: ThreadPoolTaskExecutor) = MoreExecutors.listeningDecorator(executor.threadPoolExecutor)!!
+
+  @Bean
+  fun listeningScheduler(@Qualifier("taskScheduler") scheduler: ThreadPoolTaskScheduler) = MoreExecutors.listeningDecorator(scheduler.scheduledExecutor)!!
 }
