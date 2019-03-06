26ee0144c3cee6d99d75319f5d041613d6749404
L <1252504314@qq.com>
2019-03-06 22:57:16 +0800

    update

--- sub-core/src/main/java/org/github/ops/ObjectOps.kt
+++ sub-core/src/main/java/org/github/ops/ObjectOps.kt
@@ -3,8 +3,12 @@ package org.github.ops
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.common.base.Strings.padEnd
 import com.google.common.base.Strings.padStart
+import org.apache.commons.lang3.time.FastDateFormat.getInstance
 import org.github.spring.bootstrap.AppCtxHolder.Companion.getAppCtx
+import org.github.spring.footstone.DATE_FORMAT
 import org.github.spring.footstone.EMPTY
+import org.github.spring.footstone.FORMAT_DATE
+import org.github.spring.footstone.FORMAT_TIME
 import org.springframework.cglib.beans.BeanMap.create
 import org.springframework.web.context.WebApplicationContext
 import org.springframework.web.context.request.RequestContextHolder.getRequestAttributes
@@ -25,6 +29,12 @@ val Any.map get() = create(this).toMutableMap() as MutableMap<String, Any?>
 val Any?.json get() = objectMapper.writeValueAsString(this)!!
 
 val <T: Any> T.proxy get() = appCtx.getBean(this::class.java)
+
+val String.date get() = getInstance(FORMAT_DATE).parse(this)!!
+
+val String.time get() = getInstance(FORMAT_TIME).parse(this)!!
+
+val String.dateTime get() = getInstance(DATE_FORMAT).parse(this)!!
 
 val appCtx get() = getAppCtx()
 
