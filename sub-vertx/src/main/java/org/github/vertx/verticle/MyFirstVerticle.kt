9c5e04347f4e8222740e75a726a40acd5d287d8c
L <1252504314@qq.com>
2019-03-10 11:20:24 +0800

    update

---
+++ sub-vertx/src/main/java/org/github/vertx/verticle/MyFirstVerticle.kt
@@ -0,0 +1,12 @@
+package org.github.vertx.verticle
+
+import io.vertx.core.AbstractVerticle
+
+class MyFirstVerticle: AbstractVerticle() {
+  override fun start() {
+    vertx
+      .createHttpServer()
+      .requestHandler { it.response().end("Hello world!") }
+      .listen(10001)
+  }
+}
---
+++ sub-web/src/main/java/org/github/config/VertxConfig.kt
@@ -0,0 +1,16 @@
+package org.github.config
+
+import io.vertx.core.Verticle
+import io.vertx.core.Vertx
+import org.github.vertx.verticle.MyFirstVerticle
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class VertxConfig {
+  @Bean
+  fun vertx(verticleList: List<Verticle>) = Vertx.vertx()!!.apply { verticleList.forEach { deployVerticle(it) } }
+
+  @Bean
+  fun myFirstVerticle() = MyFirstVerticle()
+}
