ada407aa015a113755eee5eb5d2587c0ecf2d170
JYDxl <1252504314@qq.com>
2019-03-11 16:53:00 +0800

    update

--- sub-core/src/main/java/org/github/ops/ObjectOps.kt
+++ sub-core/src/main/java/org/github/ops/ObjectOps.kt
@@ -3,6 +3,7 @@ package org.github.ops
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.common.base.Strings.padEnd
 import com.google.common.base.Strings.padStart
+import com.google.common.io.BaseEncoding.base16
 import org.apache.commons.lang3.time.FastDateFormat.getInstance
 import org.github.spring.bootstrap.AppCtxHolder.Companion.getAppCtx
 import org.github.spring.footstone.DATE_FORMAT
@@ -15,6 +16,7 @@ import org.springframework.web.context.request.RequestContextHolder.getRequestAt
 import org.springframework.web.context.request.ServletRequestAttributes
 import java.io.OutputStream
 import java.time.LocalDateTime
+import java.time.LocalTime
 
 fun Any?.writeValue(output: OutputStream) = objectMapper.writeValue(output, this)
 
@@ -22,14 +24,15 @@ fun String?.padStart(minLength: Int, padChar: Char) = padStart(this ?: EMPTY, mi
 
 fun String?.padEnd(minLength: Int, padChar: Char) = padEnd(this ?: EMPTY, minLength, padChar)!!
 
-fun <T: Any> Map<String, Any?>.bean(clazz: Class<T>) = clazz.getDeclaredConstructor().newInstance()!!.also { create(it).putAll(this) }
+fun <T : Any> Map<String, Any?>.bean(clazz: Class<T>) = clazz.getDeclaredConstructor().newInstance()!!.also { create(it).putAll(this) }
 
 @Suppress("UNCHECKED_CAST")
-val Any.map get() = create(this).toMutableMap() as MutableMap<String, Any?>
+val Any.map
+  get() = create(this).toMutableMap() as MutableMap<String, Any?>
 
 val Any?.json get() = objectMapper.writeValueAsString(this)!!
 
-val <T: Any> T.proxy get() = appCtx.getBean(this::class.java)
+val <T : Any> T.proxy get() = appCtx.getBean(this::class.java)
 
 val String.date get() = getInstance(FORMAT_DATE).parse(this)!!
 
@@ -38,6 +41,15 @@ val String.time get() = getInstance(FORMAT_TIME).parse(this)!!
 val String.dateTime get() = getInstance(DATE_FORMAT).parse(this)!!
 
 val LocalDateTime.value get() = "${toLocalDate()} ${toLocalTime().withNano(0)}"
+
+//TODO test
+val LocalTime.value get() = withNano(0).toString()
+
+//TODO test
+val String.hexBytes get() = base16().decode(toUpperCase())!!
+
+//TODO test
+val ByteArray.hexStr get() = base16().encode(this)!!
 
 val appCtx get() = getAppCtx()
 
@@ -48,3 +60,4 @@ val webAppCtx get() = appCtx as WebApplicationContext
 val req get() = (getRequestAttributes() as? ServletRequestAttributes)?.request
 
 val resp get() = (getRequestAttributes() as? ServletRequestAttributes)?.response
+//TODO format
--- sub-web/src/main/java/org/github/common/mapper/ICommonMapper.java
+++ sub-web/src/main/java/org/github/common/mapper/ICommonMapper.java
@@ -1,6 +1,11 @@
 package org.github.common.mapper;
 
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
 import javax.annotation.Nonnull;
 import org.github.base.entity.TipLoginLogEntity;
 import org.github.mybatis.MyBatisMapper;
@@ -9,10 +14,36 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 
 @MyBatisMapper
 public interface ICommonMapper {
-  @Select("select now()")
-  @Nonnull
-  LocalDateTime now();
+    @Select("select now()")
+    @Nonnull
+    LocalDateTime now();
 
-  @Nonnull
-  IPage<TipLoginLogEntity> page(IPage page);
+    @Nonnull
+    IPage<TipLoginLogEntity> page(IPage page);
+
+    //TODO test
+    default LocalDate localDate() {
+        return now().toLocalDate();
+    }
+
+    //TODO test
+    default LocalTime localTime() {
+        return now().toLocalTime();
+    }
+
+    //TODO test
+    default Instant instant() {
+        return now().atZone(ZoneId.systemDefault()).toInstant();
+    }
+
+    //TODO test
+    default long time() {
+        return instant().getEpochSecond();
+    }
+
+    //TODO test
+    default Date date() {
+        return Date.from(instant());
+    }
 }
+//TODO format
