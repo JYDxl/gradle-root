6eccf92c3e64ae7e072c651490f56728fd7d60b4
l <1252504314l@gmail.com>
2019-04-29 23:14:52 +0800

    异步任务及定时任务配置简化

--- sub-web/src/main/java/org/github/config/AsyncConfig.kt
+++ sub-web/src/main/java/org/github/config/AsyncConfig.kt
@@ -2,39 +2,24 @@ package org.github.config
 
 import org.github.ops.log
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler
-import org.springframework.beans.factory.ObjectProvider
-import org.springframework.boot.autoconfigure.task.TaskExecutionProperties
 import org.springframework.boot.task.TaskExecutorBuilder
-import org.springframework.boot.task.TaskExecutorCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.task.TaskDecorator
 import org.springframework.scheduling.annotation.AsyncConfigurer
 import org.springframework.scheduling.annotation.EnableAsync
 
 @Configuration
 @EnableAsync
-class AsyncConfig(private val props: TaskExecutionProperties, private val customizers: ObjectProvider<TaskExecutorCustomizer>, private val decorator: ObjectProvider<TaskDecorator>): AsyncConfigurer {
-  /** pool. */
-  private val pool = props.pool!!
+class AsyncConfig(private val builder: TaskExecutorBuilder): AsyncConfigurer {
   /** log. */
-  private val log = AsyncConfig::class.log
+  private val log = AsyncUncaughtExceptionHandler::class.log
 
   override fun getAsyncUncaughtExceptionHandler() = AsyncUncaughtExceptionHandler { ex, method, params ->
     log.error("Exception thrown by async method $method with arguments ${params.joinToString(prefix = "[", postfix = "]")}", ex)
   }
 
-  override fun getAsyncExecutor() = applicationExecutor()
+  override fun getAsyncExecutor() = taskExecutor()
 
   @Bean
-  fun applicationExecutor() = TaskExecutorBuilder()
-    .queueCapacity(pool.queueCapacity)
-    .corePoolSize(pool.coreSize)
-    .maxPoolSize(pool.maxSize)
-    .allowCoreThreadTimeOut(pool.isAllowCoreThreadTimeout)
-    .keepAlive(pool.keepAlive)
-    .threadNamePrefix(props.threadNamePrefix)
-    .customizers(customizers)
-    .taskDecorator(decorator.ifUnique)
-    .build()!!
+  fun taskExecutor() = builder.build()!!
 }
--- sub-web/src/main/java/org/github/config/SchedulingConfig.kt
+++ sub-web/src/main/java/org/github/config/SchedulingConfig.kt
@@ -1,9 +1,6 @@
 package org.github.config
 
-import org.springframework.beans.factory.ObjectProvider
-import org.springframework.boot.autoconfigure.task.TaskSchedulingProperties
 import org.springframework.boot.task.TaskSchedulerBuilder
-import org.springframework.boot.task.TaskSchedulerCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -12,15 +9,11 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar
 
 @EnableScheduling
 @Configuration
-class SchedulingConfig(private val props: TaskSchedulingProperties, private val customizers: ObjectProvider<TaskSchedulerCustomizer>): SchedulingConfigurer {
+class SchedulingConfig(private val builder: TaskSchedulerBuilder): SchedulingConfigurer {
   override fun configureTasks(registrar: ScheduledTaskRegistrar) {
-    registrar.setScheduler(applicationScheduler())
+    registrar.setScheduler(taskScheduler())
   }
 
   @Bean
-  fun applicationScheduler() = TaskSchedulerBuilder()
-    .poolSize(props.pool.size)
-    .threadNamePrefix(props.threadNamePrefix)
-    .customizers(customizers)
-    .build()!!
+  fun taskScheduler() = builder.build()!!
 }
