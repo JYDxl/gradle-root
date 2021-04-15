59dfb8703728c9bc658a757fa7ba45aba8db1b5d
l <1252504314l@gmail.com>
2021-04-15 21:47:18 +0800

    日常更新

--- sub-core/src/main/java/org/github/ops/Functions.java
+++ sub-core/src/main/java/org/github/ops/Functions.java
@@ -1,6 +1,6 @@
 package org.github.ops;
 
-import java.util.ConcurrentModificationException;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
@@ -9,7 +9,7 @@ import static java.util.Optional.*;
 
 public abstract class Functions {
   public static <T> BinaryOperator<T> throwing() {
-    return (t1, t2) -> { throw new ConcurrentModificationException();};
+    return (t1, t2) -> {throw new UnsupportedOperationException();};
   }
 
   public static <T, R> Function<T,Optional<R>> optional(@NonNull Function<T,R> function) {
@@ -18,5 +18,13 @@ public abstract class Functions {
 
   public static <T, R> Function<T,R> optional(@NonNull Function<T,R> function, R defaultValue) {
     return (t) -> ofNullable(t).map(function).orElse(defaultValue);
+  }
+
+  public static <T, R> R map(T t, @NonNull Map<T,R> map) {
+    return map(t, map, null);
+  }
+
+  public static <T, R> R map(T t, @NonNull Map<T,R> map, R defaultValue) {
+    return ofNullable(t).map(map::get).orElse(defaultValue);
   }
 }
