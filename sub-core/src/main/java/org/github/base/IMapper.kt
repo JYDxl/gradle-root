975eb1bbf1d6d86722bd53f7df721a2c778ea044
wsy <wsy@youotech.com>
2021-04-26 17:33:58 +0800

    日常更新

--- sub-core/src/main/java/org/github/base/IMapper.kt
+++ sub-core/src/main/java/org/github/base/IMapper.kt
@@ -3,5 +3,5 @@ package org.github.base
 import com.baomidou.mybatisplus.core.mapper.BaseMapper
 
 interface IMapper<T>: BaseMapper<T> {
-  fun insertBatchSomeColumn(entityList: Collection<T>): Int
+  fun insertBatchSomeColumn(entityList: List<T>): Int
 }
