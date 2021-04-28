230ca61f9a90a256cf227de0dc3b1f41d5a1b1cf
wsy <wsy@youotech.com>
2021-04-28 16:32:19 +0800

    日常更新

--- sub-core/build.gradle.kts
+++ sub-core/build.gradle.kts
@@ -1,10 +1,8 @@
 val commonscodec: String by System.getProperties()
 val mybatisplus: String by System.getProperties()
 val javassist: String by System.getProperties()
-val protobuf: String by System.getProperties()
 val byteman: String by System.getProperties()
 val netty: String by System.getProperties()
-val p6spy: String by System.getProperties()
 val mysql: String by System.getProperties()
 val jna: String by System.getProperties()
 
@@ -34,17 +32,12 @@ tasks.withType<Test> {
 }
 
 dependencies {
-  compileOnly("org.springframework.boot:spring-boot-starter-amqp")
   compileOnly("org.springframework.boot:spring-boot-starter-web")
   compileOnly("org.springframework.boot:spring-boot-starter-data-redis") {exclude(group = "io.netty")}
-  compileOnly("javax.servlet:javax.servlet-api")
 
-  compileOnly("com.baomidou:mybatis-plus-annotation:$mybatisplus")
   compileOnly("com.baomidou:mybatis-plus-extension:$mybatisplus")
-  compileOnly("com.google.protobuf:protobuf-java-util:$protobuf")
-  compileOnly("com.baomidou:mybatis-plus-core:$mybatisplus")
+
   compileOnly("io.netty:netty-all:$netty")
-  compileOnly("p6spy:p6spy:$p6spy")
 
   testImplementation("commons-codec:commons-codec:$commonscodec")
   testImplementation("org.jboss.byteman:byteman:$byteman")
