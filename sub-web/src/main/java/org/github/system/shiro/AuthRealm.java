31a77e323fea44674aa921c2d22ded87dd60dcf6
l <1252504314l@gmail.com>
2021-05-06 23:22:46 +0800

    日常更新

--- sub-web/src/main/java/org/github/system/shiro/AuthRealm.java
+++ sub-web/src/main/java/org/github/system/shiro/AuthRealm.java
@@ -12,6 +12,7 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authc.credential.CredentialsMatcher;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
@@ -19,7 +20,6 @@ import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import static com.google.common.collect.ImmutableSet.*;
 import static org.apache.commons.lang3.StringUtils.*;
-import static org.apache.shiro.util.ByteSource.Util.*;
 import static org.github.spring.bootstrap.AppCtxHolder.*;
 import static org.github.util.FuncUtil.*;
 
@@ -32,30 +32,38 @@ public class AuthRealm extends AuthorizingRealm {
   }
 
   @Override
-  protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-    val username = ((String) token.getPrincipal());
-    if (isBlank(username)) return null;
-
-    val userService = getAppCtx().getBean(ICustomUserService.class);
-    val user        = userService.queryUserInfo(username);
-    if (user == null) return null;
-
-    return new SimpleAuthenticationInfo(user, user.getPassword(), new CustomByteSource(bytes((Object) user.getSalt())), getName());
-  }
-
-  @Override
-  public String getName() {
-    return getClass().getName();
-  }
-
-  @Override
   protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-    val user        = ((UserInfoDTO) principals.getPrimaryPrincipal());
+    val user = ((UserInfoDTO) principals.getPrimaryPrincipal());
+    // val userService = getAppCtx().getBean(ICustomUserService.class);
+    // val user        = userService.queryUserInfo(username);
+    // requireNonNull(user);
     val roles       = stream(user.getRoleList()).map(RoleInfoDTO::getName).filter(Objects::nonNull).collect(toImmutableSet());
     val permissions = stream(user.getRoleList()).flatMap(Function.<RoleInfoDTO>identity().andThen(RoleInfoDTO::getPermissionList).andThen(FuncUtil::stream)).map(PermissionEntity::getName).filter(Objects::nonNull).collect(toImmutableSet());
     val info        = new SimpleAuthorizationInfo();
     info.setRoles(roles);
     info.setStringPermissions(permissions);
     return info;
+  }
+
+  @Override protected Object getAuthenticationCacheKey(PrincipalCollection principals) {
+    return ((UserInfoDTO) principals.getPrimaryPrincipal()).getUsername();
+  }
+
+  @Override
+  protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+    UsernamePasswordToken token    = (UsernamePasswordToken) authenticationToken;
+    val                   username = ((String) token.getPrincipal());
+    if (isBlank(username)) return null;
+
+    val userService = getAppCtx().getBean(ICustomUserService.class);
+    val user        = userService.queryUserInfo(username);
+    if (user == null) return null;
+
+    return new SimpleAuthenticationInfo(user, user.getPassword(), new CustomByteSource(user.getSalt()), getName());
+  }
+
+  @Override
+  public String getName() {
+    return getClass().getName();
   }
 }
--- sub-web/src/main/java/org/github/system/shiro/CustomByteSource.java
+++ sub-web/src/main/java/org/github/system/shiro/CustomByteSource.java
@@ -1,11 +1,102 @@
 package org.github.system.shiro;
 
+import java.io.File;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.util.Arrays;
+import org.apache.shiro.codec.Base64;
+import org.apache.shiro.codec.CodecSupport;
+import org.apache.shiro.codec.Hex;
 import org.apache.shiro.util.ByteSource;
-import org.apache.shiro.util.SimpleByteSource;
 
-public class CustomByteSource extends SimpleByteSource implements Serializable {
+public class CustomByteSource implements ByteSource, Serializable {
+  private byte[] bytes;
+  private String cachedHex;
+  private String cachedBase64;
+
+  public CustomByteSource() {}
+
+  public CustomByteSource(byte[] bytes) {
+    this.bytes = bytes;
+  }
+
+  public CustomByteSource(char[] chars) {
+    this.bytes = CodecSupport.toBytes(chars);
+  }
+
+  public CustomByteSource(String string) {
+    this.bytes = CodecSupport.toBytes(string);
+  }
+
   public CustomByteSource(ByteSource source) {
-    super(source);
+    this.bytes = source.getBytes();
+  }
+
+  public CustomByteSource(File file) {
+    this.bytes = (new BytesHelper()).getBytes(file);
+  }
+
+  public CustomByteSource(InputStream stream) {
+    this.bytes = (new BytesHelper()).getBytes(stream);
+  }
+
+  public int hashCode() {
+    return this.bytes != null && this.bytes.length != 0 ? Arrays.hashCode(this.bytes) : 0;
+  }
+
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    } else if (o instanceof ByteSource) {
+      ByteSource bs = (ByteSource) o;
+      return Arrays.equals(this.getBytes(), bs.getBytes());
+    } else {
+      return false;
+    }
+  }
+
+  public byte[] getBytes() {
+    return this.bytes;
+  }
+
+  public String toHex() {
+    if (this.cachedHex == null) {
+      this.cachedHex = Hex.encodeToString(this.getBytes());
+    }
+
+    return this.cachedHex;
+  }
+
+  public String toBase64() {
+    if (this.cachedBase64 == null) {
+      this.cachedBase64 = Base64.encodeToString(this.getBytes());
+    }
+
+    return this.cachedBase64;
+  }
+
+  public boolean isEmpty() {
+    return this.bytes == null || this.bytes.length == 0;
+  }
+
+  public String toString() {
+    return this.toBase64();
+  }
+
+  public static boolean isCompatible(Object o) {
+    return o instanceof byte[] || o instanceof char[] || o instanceof String || o instanceof ByteSource || o instanceof File || o instanceof InputStream;
+  }
+
+  private static final class BytesHelper extends CodecSupport {
+    private BytesHelper() {
+    }
+
+    public byte[] getBytes(File file) {
+      return this.toBytes(file);
+    }
+
+    public byte[] getBytes(InputStream stream) {
+      return this.toBytes(stream);
+    }
   }
 }
