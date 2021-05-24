5620d7d4834a7e828f16acb331c110382fd52144
wsy <wsy@youotech.com>
2021-05-24 16:35:59 +0800

    日常更新

--- sub-web-api/src/main/java/org/github/web/feign/fallback/ServiceProviderWebServerFallback.java
+++ sub-web-api/src/main/java/org/github/web/feign/fallback/ServiceProviderWebServerFallback.java
@@ -2,12 +2,7 @@ package org.github.web.feign.fallback;
 
 import org.github.spring.restful.json.JSONReturn;
 import org.github.web.feign.IServiceProviderWebServer;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 
-@ConditionalOnClass(FeignClient.class)
-@Component
 public class ServiceProviderWebServerFallback implements IServiceProviderWebServer {
     @Override
     public JSONReturn basic() {
