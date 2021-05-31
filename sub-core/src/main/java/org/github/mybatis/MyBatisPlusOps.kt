1d5c843023da4869ca9468a40df487053c4ec84f
wsy <wsy@youotech.com>
2021-05-31 15:00:44 +0800

    日常更新

---
+++ sub-core/src/main/java/org/github/mybatis/MyBatisPlusOps.kt
@@ -0,0 +1,12 @@
+package org.github.mybatis
+
+import com.baomidou.mybatisplus.extension.service.IService
+import com.baomidou.mybatisplus.extension.service.IService.DEFAULT_BATCH_SIZE
+import com.google.common.collect.Lists.partition
+import org.github.base.IMapper
+
+fun <T> IService<T>.insertBatch(entityList: List<T>): Int {
+    val mapper = baseMapper as IMapper<T>
+    val partition: List<List<T>> = partition(entityList, DEFAULT_BATCH_SIZE)
+    return partition.sumOf {mapper.insertBatchSomeColumn(it)}
+}
