e4bd978a57c30c0a68578e499a5c0cee8a262ba8
wsy <wsy@youotech.com>
2021-06-02 14:55:27 +0800

    日常更新

--- sub-auth-api/src/main/java/org/github/auth/feign/IServiceProviderAuthServer.kt
+++ sub-auth-api/src/main/java/org/github/auth/feign/IServiceProviderAuthServer.kt
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.github.spring.restful.json.JSONDataReturn
 import org.github.auth.shiro.model.UserDTO
-import org.github.constant.Constants.SERVICE_PROVIDER_AUTH_SERVER
 import org.github.shiro.AuthorInfo
 import org.github.spring.restful.json.JSONArrayReturn
 
-@FeignClient(SERVICE_PROVIDER_AUTH_SERVER)
+@FeignClient("auth-server")
 interface IServiceProviderAuthServer {
   @GetMapping("/public/security/user/{username}")
   fun user(@PathVariable("username") username: String): JSONDataReturn<UserDTO>
