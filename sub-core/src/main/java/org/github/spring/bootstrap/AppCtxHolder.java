0a38fcf004f67f393531d3864ea570c1c2230641
wsy <wsy@youotech.com>
2021-06-02 09:56:05 +0800

    日常更新

--- sub-core/src/main/java/org/github/spring/bootstrap/AppCtxHolder.java
+++ sub-core/src/main/java/org/github/spring/bootstrap/AppCtxHolder.java
@@ -4,9 +4,7 @@ import lombok.NonNull;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
 
-@Component
 public class AppCtxHolder implements ApplicationContextAware {
   @Override
   public void setApplicationContext(@NonNull ApplicationContext context) throws BeansException {
