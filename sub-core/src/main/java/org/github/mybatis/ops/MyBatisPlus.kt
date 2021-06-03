89d0ed94b44ff557bf3a613df2cc75379945bbeb
wsy <wsy@youotech.com>
2021-06-03 18:33:42 +0800

    日常更新

--- sub-core/src/main/java/org/github/mybatis/ops/MyBatisPlus.kt
+++ sub-core/src/main/java/org/github/mybatis/ops/MyBatisPlus.kt
@@ -9,7 +9,7 @@ import com.google.common.collect.Lists.partition
 import org.github.base.IMapper
 import org.github.base.IPage
 
-fun <T> IService<T>.insertBatch(entityList: List<T>): Int {
+fun <T : Any> IService<T>.insertBatch(entityList: List<T>): Int {
   val mapper = baseMapper as IMapper<T>
   val partition: List<List<T>> = partition(entityList, DEFAULT_BATCH_SIZE)
   return partition.sumOf { mapper.insertBatchSomeColumn(it) }
@@ -19,12 +19,14 @@ fun <T : Any> IService<T>.ktQueryWrapper(): KtQueryChainWrapper<T> = ktQuery()
 
 fun <T : Any> IService<T>.ktUpdateWrapper(): KtUpdateChainWrapper<T> = ktUpdate()
 
+fun <T : Any> IService<T>.ktGetById(id: java.io.Serializable): T? = getById(id)
+
+fun <T : Any> IService<T>.ktListByIds(ids: List<java.io.Serializable>): MutableList<T> = if (ids.isEmpty()) arrayListOf() else listByIds(ids)
+
 fun <T : Any> ChainQuery<T>.ktPage(page: IPage<T>): IPage<T> = page(page)
 
 fun <T : Any> ChainQuery<T>.ktList(): MutableList<T> = list()
 
 fun <T : Any> ChainQuery<T>.ktInList(`in`: List<Any>): MutableList<T> = if (`in`.isEmpty()) arrayListOf() else list()
 
-fun <T : Any> IService<T>.ktGetById(id: java.io.Serializable): T? = getById(id)
-
-fun <T : Any> IService<T>.ktListByIds(ids: List<java.io.Serializable>): MutableList<T> = if (ids.isEmpty()) arrayListOf() else listByIds(ids)
+fun <T : Any> ChainQuery<T>.ktInPage(`in`: List<Any>, page: IPage<T>): IPage<T> = if (`in`.isEmpty()) page else ktPage(page)
