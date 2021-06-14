66e6966e36d5f178257eb0494741a75dbb8a87e4
l <1252504314l@gmail.com>
2021-06-14 22:21:04 +0800

    日常更新

--- sub-core/src/main/java/org/github/ops/Bean.kt
+++ sub-core/src/main/java/org/github/ops/Bean.kt
@@ -1,5 +1,8 @@
 package org.github.ops
 
 import cn.hutool.core.bean.BeanUtil.trimStrFields
+import org.github.exception.ParamsErrorException
 
 fun <T: Any> T.trimStrFields(vararg ignore: String = emptyArray()): T = trimStrFields(this, *ignore)
+
+fun <T> T?.notNullAnd(predicate: (T) -> Boolean, msg: () -> String?) = this?.takeIf(predicate) ?: throw ParamsErrorException(msg().toString())
