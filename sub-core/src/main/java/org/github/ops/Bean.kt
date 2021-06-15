f882017be3520779f7dbf6f50a49ffc4a34738f2
wsy <wsy@youotech.com>
2021-06-15 11:30:54 +0800

    日常更新

--- sub-core/src/main/java/org/github/ops/Bean.kt
+++ sub-core/src/main/java/org/github/ops/Bean.kt
@@ -1,8 +1,11 @@
 package org.github.ops
 
+import cn.hutool.core.bean.BeanUtil.copyProperties
 import cn.hutool.core.bean.BeanUtil.trimStrFields
 import org.github.exception.ParamsErrorException
 
 fun <T: Any> T.trimStrFields(vararg ignore: String = emptyArray()): T = trimStrFields(this, *ignore)
 
 fun <T> T?.notNullAnd(predicate: (T) -> Boolean, msg: () -> String?) = this?.takeIf(predicate) ?: throw ParamsErrorException(msg().toString())
+
+fun <T, R: T> R.copy(entity: T) = apply { copyProperties(entity, this) }
