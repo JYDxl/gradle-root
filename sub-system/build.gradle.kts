477ee8af993c61b7ac59b3350aa48e8390c74135
wsy <wsy@youotech.com>
2021-06-21 08:55:44 +0800

    日常更新

--- sub-system/build.gradle.kts
+++ sub-system/build.gradle.kts
@@ -1,3 +1,4 @@
+val mybatisplus: String by System.getProperties()
 val shiroredis: String by System.getProperties()
 val swagger: String by System.getProperties()
 val shiro: String by System.getProperties()
@@ -11,6 +12,8 @@ dependencies {
 
   compileOnly("io.springfox:springfox-boot-starter:$swagger")
   compileOnly("org.apache.shiro:shiro-spring-boot-web-starter:$shiro")
+
+  compileOnly("com.baomidou:mybatis-plus-boot-starter:$mybatisplus")
 
   compileOnly("org.crazycake:shiro-redis-spring-boot-starter:$shiroredis") {
     exclude(group = "org.apache.maven.plugins")
