1020a6d840bf3217459b0ed83aaa643808fa5e9f
l <1252504314l@gmail.com>
2021-06-29 00:01:41 +0800

    日常更新

--- sub-system-config/src/main/java/org/github/config/MyBatisConfig.kt
+++ sub-system-config/src/main/java/org/github/config/MyBatisConfig.kt
@@ -1,6 +1,7 @@
 package org.github.config
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler
+import com.baomidou.mybatisplus.core.incrementer.DefaultIdentifierGenerator
 import com.baomidou.mybatisplus.core.injector.AbstractMethod
 import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector
 import com.baomidou.mybatisplus.extension.injector.methods.InsertBatchSomeColumn
@@ -9,12 +10,15 @@ import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerIntercep
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor
 import org.apache.ibatis.reflection.MetaObject
+import org.github.base.IEntity
 import org.github.mybatis.MyBatisMapper
 import org.github.shiro.ops.DEFAULT_VALUE_ANONYMOUS_ID
 import org.github.shiro.ops.userOrNull
 import org.mybatis.spring.annotation.MapperScan
+import org.springframework.cloud.commons.util.InetUtils
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.net.InetAddress
 import java.time.LocalDateTime
 import java.time.LocalDateTime.now
 
@@ -34,6 +38,9 @@ class MyBatisConfig {
 
   @Bean
   fun customMetaObjectHandler() = CustomMetaObjectHandler()
+
+  @Bean
+  fun customIdentifierGenerator(@Suppress("SpringJavaInjectionPointsAutowiringInspection") net: InetUtils) = CustomIdentifierGenerator(net.findFirstNonLoopbackAddress())
 }
 
 class CustomSqlInjector: DefaultSqlInjector() {
@@ -54,4 +61,16 @@ class CustomMetaObjectHandler: MetaObjectHandler {
   }
 
   private val id: Long get() = userOrNull?.id ?: DEFAULT_VALUE_ANONYMOUS_ID
+}
+
+class CustomIdentifierGenerator(addr: InetAddress): DefaultIdentifierGenerator(addr) {
+  override fun nextId(entity: Any): Long {
+    entity as IEntity
+    return entity.pkVal() as Long? ?: super.nextId(entity)
+  }
+
+  override fun nextUUID(entity: Any): String {
+    entity as IEntity
+    return entity.pkVal() as String? ?: super.nextUUID(entity)
+  }
 }
