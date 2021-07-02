0fb83d4e06353b43738a1882502b12f95adff689
l <1252504314l@gmail.com>
2021-07-02 11:39:32 +0800

    日常更新

--- sub-cloud-gateway/build.gradle.kts
+++ sub-cloud-gateway/build.gradle.kts
@@ -53,6 +53,10 @@ val netty: String by System.getProperties()
 dependencies {
   implementation("io.netty:netty-all:$netty")
 
+  implementation("com.alibaba.cloud:spring-cloud-starter-alibaba-sentinel") {exclude(group = "io.netty")}
+  implementation("com.alibaba.cloud:spring-cloud-alibaba-sentinel-gateway")
+  implementation("com.alibaba.csp:sentinel-datasource-nacos")
+
   implementation("com.alibaba.cloud:spring-cloud-starter-alibaba-nacos-discovery")
   implementation("com.alibaba.cloud:spring-cloud-starter-alibaba-nacos-config")
   implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
