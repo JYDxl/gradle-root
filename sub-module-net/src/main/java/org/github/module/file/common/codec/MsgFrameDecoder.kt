889f408a17092a1952b6952ff1b033afcd0ef486
l <1252504314l@gmail.com>
2021-12-31 15:34:10 +0800

    日常更新

--- sub-module-net/src/main/java/org/github/module/file/common/codec/MsgFrameDecoder.kt
+++ sub-module-net/src/main/java/org/github/module/file/common/codec/MsgFrameDecoder.kt
@@ -8,14 +8,21 @@ import org.github.netty.ops.info
 import org.github.ops.info
 import org.github.ops.log
 import org.github.ops.warn
+import org.slf4j.Logger
 import java.util.concurrent.TimeUnit.MINUTES
 
-class MsgFrameDecoder: LengthDecoder(1, 8, MAX_LENGTH.toInt(), getLength = {buf, _, _ -> buf.getLong(1)}, log = MsgFrameDecoder::class.log) {
+class MsgFrameDecoder: LengthDecoder(1, 8, MAX_LENGTH.toInt(), getLength = {buf, _, _ -> buf.getLong(1)}) {
+  override val log: Logger get() = logger
+
   override fun channelActive(ctx: ChannelHandlerContext) {
     val channel = ctx.channel()
     log.info {"对象【${channel.info}】上线"}
     channel.closeFuture().addListener {log.warn {"对象【${channel.info}】下线"}}
     ctx.executor().schedule({if (channel.isActive && !channel.hasMark) channel.close()}, 1, MINUTES)
     super.channelActive(ctx)
+  }
+
+  companion object {
+    private val logger = MsgFrameDecoder::class.log
   }
 }
