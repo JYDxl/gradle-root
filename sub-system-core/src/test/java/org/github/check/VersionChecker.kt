89a526630cbe00ea13908dfc4bc69e817a112056
l <1252504314l@gmail.com>
2022-01-01 12:44:57 +0800

    日常更新

--- sub-system-core/src/test/java/org/github/check/VersionChecker.kt
+++ sub-system-core/src/test/java/org/github/check/VersionChecker.kt
@@ -8,7 +8,7 @@ import org.github.ops.warn
 fun main() {
   val versions = """"""
   val multiset: ImmutableMultiset<String> = versions.split('\n').stream().map {it.substringBeforeLast(':')}.collect(toImmutableMultiset())
-  multiset.forEachEntry {dep, count -> if (count > 1) log.warn {dep.substringAfter(':')}}
+  multiset.forEachEntry {dep: String, count -> if (count > 1) log.warn {dep.substringAfter(':')}}
 }
 
 private val log = "main".log
