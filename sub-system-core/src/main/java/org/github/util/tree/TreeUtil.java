908920f1b0ad86bc3a581a0161ecc6b4f3692a37
l <1252504314l@gmail.com>
2022-03-08 09:22:55 +0800

    日常更新

--- sub-system-core/src/main/java/org/github/util/tree/TreeUtil.java
+++ sub-system-core/src/main/java/org/github/util/tree/TreeUtil.java
@@ -5,10 +5,11 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import lombok.*;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
-import static com.google.common.collect.Lists.*;
+import com.google.common.collect.TreeMultimap;
 import static com.google.common.collect.Multimaps.*;
-import static com.google.common.collect.TreeMultimap.*;
+import static java.util.Comparator.*;
 import static java.util.Optional.*;
 import static org.github.util.FuncUtil.*;
 
@@ -21,20 +22,23 @@ public abstract class TreeUtil {
     return root;
   }
 
-  public static <T extends TreeNode<I,E>, I, E> @NonNull List<T> buildTree(@NonNull List<T> list, I pid, @NonNull Comparator<Optional<I>> keyComparator, @NonNull Comparator<T> valueComparator) {
+  public static <T extends TreeNode<I,E>, I extends Comparable<? super I>, E> @NonNull List<T> buildTree(@NonNull List<T> list, I pid, @NonNull Comparator<T> valueComparator) {
     val func     = optional(T::getPid);
-    val index    = create(keyComparator, valueComparator);
     val multimap = index(list, func::apply);
+
+    val keyComparator = Comparator.<Optional<I>,I>comparing(v -> v.orElse(null), nullsFirst(naturalOrder()));
+    val index         = TreeMultimap.create(keyComparator, valueComparator);
     index.putAll(multimap);
+
     val root = index.get(ofNullable(pid));
     recursion(root, index);
-    return newArrayList(root);
+    return Lists.newArrayList(root);
   }
 
   private static <T extends TreeNode<I,E>, I, E> void recursion(Collection<T> list, Multimap<Optional<I>,T> index) {
     for (T item : list) {
       val id  = item.getId();
-      val sub = index.get(of(id));
+      val sub = index.get(Optional.of(id));
       if (sub.isEmpty()) continue;
       item.setChild(sub);
       recursion(sub, index);
