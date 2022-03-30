fe15b5b715c40b31a7de87d31bee1d4a500927c8
l <1252504314l@gmail.com>
2022-03-30 14:56:09 +0800

    日常更新

--- sub-system-core/src/test/resources/logback.groovy
+++
@@ -1,8 +0,0 @@
-def out = "%d %5level --- [%25.25thread] %-30.30logger{30} : %msg%n"
-
-appender("console", ConsoleAppender) {
-  encoder(PatternLayoutEncoder) { pattern = out }
-}
-
-logger("org.github", TRACE)
-root(DEBUG, ["console"])
