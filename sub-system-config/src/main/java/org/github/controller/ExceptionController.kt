611b61e42a807433e1da362e1115625313759c66
l <1252504314l@gmail.com>
2022-11-06 23:08:49 +0800

    日常更新

--- sub-system-config/src/main/java/org/github/controller/ExceptionController.kt
+++ sub-system-config/src/main/java/org/github/controller/ExceptionController.kt
@@ -1,14 +1,13 @@
 package org.github.controller
 
 import org.github.exception.ClientException
+import org.github.exception.ServerException
 import org.github.ops.error
 import org.github.ops.log
 import org.github.spring.restful.json.JSONReturn
-import org.springframework.http.ResponseEntity.badRequest
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import javax.validation.ConstraintViolationException
 
 @RestControllerAdvice
 class ExceptionController {
@@ -17,11 +16,12 @@ class ExceptionController {
   @ExceptionHandler(Throwable::class)
   fun handleThrowable(e: Throwable) = JSONReturn.error(null).apply {log.error(e) {}}
 
-  @ExceptionHandler(ClientException::class, ConstraintViolationException::class)
-  fun handleParamsErrorException(e: Exception) = badRequest().apply {log.error(e) {}}
+  @ExceptionHandler(ClientException::class)
+  fun handleClientException(e: ClientException) = JSONReturn.param(e.message).apply {log.error(e) {}}
+
+  @ExceptionHandler(ServerException::class)
+  fun handleServerException(e: ServerException) = JSONReturn.error(e.message).apply {log.error(e) {}}
 
   @ExceptionHandler(MethodArgumentNotValidException::class)
-  fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): JSONReturn {
-    return JSONReturn.param(e.bindingResult.allErrors[0].defaultMessage)
-  }
+  fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException) = JSONReturn.param(e.bindingResult.allErrors[0].defaultMessage).apply {log.error(e) {}}
 }
