ca54599b8765bc1e8de8a1f8a8c6844f27a06a72
l <1252504314l@gmail.com>
2022-11-08 21:19:53 +0800

    日常更新

--- sub-module-web-dto/src/main/java/org/github/web/module/index/RegisterBo.java
+++ sub-module-web-dto/src/main/java/org/github/web/module/index/RegisterBo.java
@@ -3,6 +3,7 @@ package org.github.web.module.index;
 import javax.validation.constraints.NotNull;
 import lombok.*;
 import org.github.base.IParam;
+import static cn.hutool.crypto.symmetric.SymmetricAlgorithm.*;
 
 @Data
 public class RegisterBo implements IParam {
@@ -11,4 +12,7 @@ public class RegisterBo implements IParam {
 
   @NotNull(message = "密码不能为空")
   private String password;
+
+  @NotNull(message = "加密方式不能为空")
+  private String algorithm = AES.getValue();
 }
