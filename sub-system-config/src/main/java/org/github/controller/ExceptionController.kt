4d9c00197110ec55df4a4f2b96133e5c04d091bf
l <1252504314l@gmail.com>
2022-11-22 23:37:09 +0800

    日常更新

--- sub-system-config/src/main/java/org/github/controller/ExceptionController.kt
+++ sub-system-config/src/main/java/org/github/controller/ExceptionController.kt
@@ -1,15 +1,16 @@
 package org.github.controller
 
 import cn.dev33.satoken.exception.*
-import com.google.common.net.HttpHeaders
+import com.google.common.net.HttpHeaders.CONTENT_TYPE
 import org.github.exception.ExternalException
 import org.github.exception.InternalException
 import org.github.exception.RemoteException
 import org.github.ops.error
 import org.github.ops.log
 import org.github.spring.restful.json.JSONReturn
+import org.github.spring.restful.json.JSONReturn.*
 import org.springframework.http.HttpEntity
-import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -20,28 +21,28 @@ class ExceptionController {
   private val log = ExceptionController::class.log
 
   @ExceptionHandler(Throwable::class)
-  fun handleThrowable(e: Throwable) = JSONReturn.internal(null).apply {log.error(e) {}}.entity
+  fun handleThrowable(e: Throwable) = internal(null).apply {log.error(e) {}}.entity
 
   @ExceptionHandler(ExternalException::class)
-  fun handleExternalException(e: ExternalException) = JSONReturn.external(e.message).entity
+  fun handleExternalException(e: ExternalException) = external(e.message).entity
 
   @ExceptionHandler(InternalException::class)
-  fun handleInternalException(e: InternalException) = JSONReturn.internal(e.message).entity
+  fun handleInternalException(e: InternalException) = internal(e.message).entity
 
   @ExceptionHandler(RemoteException::class)
   fun handleRemoteException(e: RemoteException) = e.data.entity
 
   @ExceptionHandler(NotLoginException::class)
-  fun handleAuthException(e: Exception) = JSONReturn.auth(e.message).entity
+  fun handleAuthException(e: Exception) = auth(e.message).entity
 
   @ExceptionHandler(NotPermissionException::class, NotRoleException::class, NotSafeException::class)
-  fun handlePermException(e: Exception) = JSONReturn.perm(e.message).entity
+  fun handlePermException(e: Exception) = perm(e.message).entity
 
   @ExceptionHandler(MethodArgumentNotValidException::class)
-  fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException) = JSONReturn.external(e.bindingResult.allErrors.map {it.defaultMessage}.joinToString("；")).entity
+  fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException) = external(e.bindingResult.allErrors.map {it.defaultMessage}.joinToString("；")).entity
 
   @ExceptionHandler(ConstraintViolationException::class)
-  fun handleConstraintViolationException(e: ConstraintViolationException) = JSONReturn.external(e.constraintViolations.joinToString("；") {it.message}).entity
+  fun handleConstraintViolationException(e: ConstraintViolationException) = external(e.constraintViolations.joinToString("；") {it.message}).entity
 
-  private val JSONReturn.entity: HttpEntity<String> get() = ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, mediaType().toString()).body(toString())
+  private val JSONReturn.entity: HttpEntity<String> get() = ok().header(CONTENT_TYPE, mediaType().toString()).body(toString())
 }
