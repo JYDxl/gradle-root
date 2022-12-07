993130ca4b1a82ce54b4c1726206a90cc668b44f
hanjian <18355257237@163.com>
2022-12-07 10:52:26 +0800

    日常更新

--- sub-system-common/src/main/java/org/github/config/FeignConfig.kt
+++ sub-system-common/src/main/java/org/github/config/FeignConfig.kt
@@ -1,12 +1,9 @@
 package org.github.config
 
 import feign.Logger.Level.FULL
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
-import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@ConditionalOnClass(EnableFeignClients::class)
 @Configuration
 class FeignConfig {
   @Bean
